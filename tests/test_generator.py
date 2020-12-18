11289eac629bb6e56686ff151b4d55f8f3f276b1
gijs.schouten <gijs.schouten@7TX7HC2>
2020-12-18 14:19:39 +0100

    ptlint2

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,6 +6,7 @@ def test_sample_single_word():
     word = generator.sample(woorden_tuple)
     assert word in woorden_tuple
 
+
 def test_sample_multiple_words():
     woorden_tuple = ('foo', 'bar', 'foobar')
     words = generator.sample(woorden_tuple, 2)
@@ -13,6 +14,7 @@ def test_sample_multiple_words():
     assert words[0] in woorden_tuple
     assert words[1] in woorden_tuple
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
